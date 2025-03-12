3bff74d5dc8122db0856053d047d7d07ecf83ce1
Aman Tuladhar <atuladhar.aman@gmail.com>
2025-03-12 08:12:54 -0500

    Add actuator dependency to expose refresh endpoint

--- catalogservice/src/main/java/dev/babal/catalogservice/config/PolarProperties.java
+++ catalogservice/src/main/java/dev/babal/catalogservice/config/PolarProperties.java
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @EnableConfigurationProperties
-@ConfigurationProperties(prefix = "polar")
+@ConfigurationProperties(prefix = "polar") // Already listens to RefreshScopeRefreshedEvent
 public record PolarProperties(String greeting) {
 
 }
