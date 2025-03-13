20e1a82843cd4cbf9d1e60a801fb8e35e2668e63
Aman Tuladhar <atuladhar.aman@gmail.com>
2025-03-13 09:21:32 -0500

    Add code for order-service

---
+++ orders-service/src/test/java/dev/babal/ordersservice/TestOrdersServiceApplication.java
@@ -0,0 +1,11 @@
+package dev.babal.ordersservice;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestOrdersServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(OrdersServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
