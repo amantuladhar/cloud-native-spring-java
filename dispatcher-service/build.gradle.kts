d89fc3f104f0186e3915d319c22f8049b52bdaca
Aman Tuladhar <atuladhar.aman@gmail.com>
2025-03-15 20:16:04 -0500

    Use spring cloud stream - and configure exchange topic names for cloud/stream functions

--- dispatcher-service/build.gradle.kts
+++ dispatcher-service/build.gradle.kts
@@ -1,40 +1,54 @@
 plugins {
-	java
-	id("org.springframework.boot") version "3.4.3"
-	id("io.spring.dependency-management") version "1.1.7"
+    java
+    id("org.springframework.boot") version "3.4.3"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "dev.babal"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(17)
-	}
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 extra["springCloudVersion"] = "2024.0.0"
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
-	implementation("org.springframework.cloud:spring-cloud-function-context")
+//	implementation("org.springframework.boot:spring-boot-starter")
+//	implementation("org.springframework.cloud:spring-cloud-function-context")
+    // ^^^____ included on cloud stream
+    implementation("org.springframework.cloud:spring-cloud-starter-stream-rabbit")
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.springframework.cloud:spring-cloud-stream-test-binder")
+    testCompileOnly("org.projectlombok:lombok")
+    testAnnotationProcessor("org.projectlombok:lombok")
+
+//    testImplementation("org.springframework.cloud:spring-cloud-stream")
+//        artifact {
+//            name = "spring-cloud-stream"
+//            extension = "jar"
+//            type = "test-jar"
+//            classifier = "test-binder"
+//        }
+//    }
 }
 
 dependencyManagement {
-	imports {
-		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
-	}
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
